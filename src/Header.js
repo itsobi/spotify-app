d02a61e0f0c20d2a8dd125e4a003627cec76461c
itsobi <obiobialo@aol.com>
2022-02-16 11:56:52 -0600

    added header to app

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div style={{ textAlign: "center", padding: "10px 0" }}>
+      <h1>Obi's Music App</h1>
+    </div>
+  );
+};
+
+export default Header;
