00437696c804b50c7c392c37ea3d5a57bc6b6155
itsobi <obiobialo@aol.com>
2022-02-16 11:55:55 -0600

    correctly rendered data to screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "./Header";
 import SearchBar from "./SearchBar";
 
 const App = () => {
   return (
     <div className="ui container">
+      <Header />
       <SearchBar />
     </div>
   );
--- src/Track.js
+++ src/Track.js
@@ -1,16 +1,27 @@
 import React from "react";
 
 const Track = ({ song }) => {
-  console.log(song);
   return (
     <div className="ui relaxed divided list">
       <div className="item">
-        <img src={song.image} alt="song image" />
+        <img
+          src={song.image}
+          style={{ borderRadius: "3px" }}
+          alt="song image"
+        />
         <div className="content">
-          <a href={song.link} className="header">
+          <a
+            href={song.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="header"
+          >
             {song.title}
           </a>
-          <div className="description">{song.artist}</div>
+          <div className="description">
+            <h4>{song.artist}</h4>
+          </div>
+          <hr style={{ height: "1px", backgroundColor: "#eee" }} />
         </div>
       </div>
     </div>
